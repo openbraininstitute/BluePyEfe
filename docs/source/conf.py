2d110c3a1f2fd060d251d121bb327a9f6171c460
Daniel Fernández <daferoes@gmail.com>
2026-08-21 11:20:34 +0200

    Migrate BluePyEfe to OBI Sphinx theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -118,18 +118,19 @@ suppress_warnings = [
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinx-bluebrain-theme'
-html_title = 'BluepyEfe'
+html_theme = "obi_sphinx_theme"
+html_title = "BluepyEfe"
 html_show_sourcelink = False
 html_theme_options = {
-    "repo_url": "https://github.com/openbraininstitute/BluePyEfe/",
-    "repo_name": "openbraininstitute/BluePyEfe"
+    "icon_links": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/openbraininstitute/BluePyEfe",
+            "icon": "fa-brands fa-github",
+        },
+    ],
+    "navbar_align": "left",
 }
-
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-#html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = ['./']
